942eed4cd009c9b87bfc84d9eb69d9ca949c3e77
hussainferoz <hussainferoz839@gmail.com>
2020-06-20 23:08:10 +0500

    added styles as a props to styled components.

--- Styles.js
+++ Styles.js
@@ -2,30 +2,30 @@ import Styled from 'styled-components/native';
 
 export const Container = Styled.View`
     flex: 1;
-    background-color: #fff;
+    background-color: ${(props) => props.theme.BACKGROUND_COLOR};
     align-items: center;
     justify-content: center;
 `;
 
 export const TextContainer = Styled.View`
-    border: 1px solid #121212;
+    border: 1px solid ${(props) => props.theme.TEXT_COLOR};
     padding: 16px 40px;
     border-radius: 8px;
 `;
 
 export const Text = Styled.Text`
-    color: #121212;
+    color: ${(props) => props.theme.TEXT_COLOR};
     font-size: 28px;
 `;
 
 export const ButtonContainer = Styled.TouchableOpacity`
     margin: 30px 0;
-    background-color: #23a8d9;
+    background-color: ${(props) => props.theme.BUTTON_COLOR};
     padding: 16px 40px;
     border-radius: 8px;
 `;
 
 export const ButtonText = Styled.Text`
+    color: ${(props) => props.theme.BUTTON_TEXT_COLOR};
     font-size: 18px;
-    color: #121212;
 `;
--- screens/Home.js
+++ screens/Home.js
@@ -1,18 +1,36 @@
 import React from 'react';
 
+import { ThemeProvider } from 'styled-components';
+import { useSelector, useDispatch } from 'react-redux';
+
 import { Container, TextContainer, Text, ButtonContainer, ButtonText } from '../Styles';
+import { switchTheme } from '../redux/ThemeActions';
+import { lightTheme, darkTheme } from '../Theme';
 
 const Home = () => {
-	return (
-		<Container>
-			<TextContainer>
-				<Text>This is a light theme!</Text>
-			</TextContainer>
+	const theme = useSelector((state) => state.themeReducer.theme);
+	const dispatch = useDispatch();
 
-			<ButtonContainer>
-				<ButtonText>Switch to dark theme</ButtonText>
-			</ButtonContainer>
-		</Container>
+	const { mode } = theme;
+
+	return (
+		<ThemeProvider theme={theme}>
+			<Container>
+				<TextContainer>
+					{mode === 'light' ? <Text>This is a light theme!</Text> : <Text>This is a dark theme!</Text>}
+				</TextContainer>
+
+				{mode === 'light' ? (
+					<ButtonContainer onPress={() => dispatch(switchTheme(darkTheme))}>
+						<ButtonText>Switch to dark theme</ButtonText>
+					</ButtonContainer>
+				) : (
+					<ButtonContainer onPress={() => dispatch(switchTheme(lightTheme))}>
+						<ButtonText>Switch to light theme</ButtonText>
+					</ButtonContainer>
+				)}
+			</Container>
+		</ThemeProvider>
 	);
 };
 
